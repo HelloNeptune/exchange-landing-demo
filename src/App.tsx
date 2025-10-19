f0c09df0b00b903abf83407f01e1551363c3dcbf
HelloNeptune <ahmet.burak10@gmail.com>
2025-10-19 03:46:14 +0100

    EXL: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import styles from "./app.module.scss";
+
+export default function App() {
+  return (
+    <div className={styles.app}>
+      <main className={styles.main}>
+        Exchange Landing Demo
+      </main>
+      <footer className={styles.footer}>
+        <div className={`container ${styles['footer-container']}`}>
+          <p className={styles['footer-text']}>
+            © 2025 <b>EASYEXCHANGE</b>. All rights reserved.
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
